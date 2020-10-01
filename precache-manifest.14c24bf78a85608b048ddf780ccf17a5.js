1b0ab92ab3079ac7657316afbf2ae872e1de8ee9
jeremy albuixech <jeremy.albuixech@gmail.com>
2020-10-01 11:34:55 -0700

    Added default variation values

---
+++ precache-manifest.14c24bf78a85608b048ddf780ccf17a5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cc89f899b255fdca9f0608cce6dca69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be630c0c827167743735",
+    "url": "/static/css/2.d54bb455.chunk.css"
+  },
+  {
+    "revision": "be630c0c827167743735",
+    "url": "/static/js/2.9a90bf44.chunk.js"
+  },
+  {
+    "revision": "c346956c1449420dcbe7f2a51cdfc9c4",
+    "url": "/static/js/2.9a90bf44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac0fd0053d92005a69ab",
+    "url": "/static/js/3.32a5e22a.chunk.js"
+  },
+  {
+    "revision": "d51c699b4357cb37dcc86d11129f128d",
+    "url": "/static/js/3.32a5e22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7df2abe480460d4985dd",
+    "url": "/static/js/4.3dfda650.chunk.js"
+  },
+  {
+    "revision": "4983a424d41bb18c9f30",
+    "url": "/static/js/5.128515e7.chunk.js"
+  },
+  {
+    "revision": "ea3b6b279110804e5baa",
+    "url": "/static/js/main.3ce0f845.chunk.js"
+  },
+  {
+    "revision": "89f9cd5cdcce5e573e58",
+    "url": "/static/js/runtime-main.2f65b0dd.js"
+  },
+  {
+    "revision": "53fe8376c4f4de47d2c3b56b979b59fd",
+    "url": "/static/media/documentation-icon.53fe8376.svg"
+  },
+  {
+    "revision": "d48d545cffba6175b3cbb39099d82c57",
+    "url": "/static/media/github-icon.d48d545c.svg"
+  }
+]);
